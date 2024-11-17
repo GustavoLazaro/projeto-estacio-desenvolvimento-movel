c22f0b36f69511ee5671a5e2f4282ca5a6576570
Gustavo Hermano Lázaro da Silva <89167937+GustavoLazaro@users.noreply.github.com>
2024-11-17 16:52:19 -0300

    Add files via upload

---
+++ react-mobile-shop/src/Components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <h2>Mobile Shop | E-Commerce Project</h2>
+      <p>
+        &copy; <span>{year}</span> MobileShop.com - All Rights Copyright
+        Reserved To{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/gustavolazaro/"
+        > Gustavo Lázaro
+        </a>
+      </p>
+    </footer>
+  );
+}
